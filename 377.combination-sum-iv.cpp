e37f3d683584318d8d87b58fdc37bf85bf3cbbb4
Jesse Chu <jesse.chu@outlook.com>
2025-08-12 23:11:32 -0700

    LeetCode #377

---
+++ 377.combination-sum-iv.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=377 lang=cpp
+ *
+ * [377] Combination Sum IV
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned long long> dp(target + 1, 0);
+        dp[0] = 1;
+        for(int j = 1; j <= target; j ++){
+            for(int i = 0; i< nums.size(); i ++){
+                if(j >= nums[i]){
+                    dp[j] += dp[j - nums[i]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+// @lc code=end
+
