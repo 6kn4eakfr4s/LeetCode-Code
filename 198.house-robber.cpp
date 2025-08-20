6d37dec640f5dcc831ec08c7af2d74f36deb870d
Jesse Chu <jesse.chu@outlook.com>
2025-08-20 11:04:30 -0400

    LeetCode #198

---
+++ 198.house-robber.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        //dp[i] means the max value from 0-i houses (including i);
+        //dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        if(nums.size() == 1)
+            return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); i ++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.size() - 1];
+    }
+};
+// @lc code=end
+
