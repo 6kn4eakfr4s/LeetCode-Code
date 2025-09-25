6826069bd48e68ed49ee51a72ff13ff064e26752
Jesse Chu <jesse.chu@outlook.com>
2025-09-25 16:49:57 -0400

    LeetCode #300

---
+++ 300.longest-increasing-subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=300 lang=cpp
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        int result = 0;
+        for(int i = 0; i < nums.size(); i ++){
+            for(int j = 0; j < i; j ++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            result = max(result, dp[i]);
+        }
+        return result;
+        
+    }
+};
+// @lc code=end
+
