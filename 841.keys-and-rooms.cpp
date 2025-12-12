45a09be95452e947be435011fca8a895a0df7936
Jesse Chu <jesse.chu@outlook.com>
2025-12-12 09:17:38 -0500

    LeetCode #841

---
+++ 841.keys-and-rooms.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=841 lang=cpp
+ *
+ * [841] Keys and Rooms
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+      vector<bool> visited(rooms.size(), false);  
+      int roomsLeft = rooms.size() - 1;
+      visited[0] = true; 
+      queue<int> q;
+      q.push(0);
+      while(!q.empty()){
+        int f = q.front();
+        q.pop();
+        for(int i = 0; i < rooms[f].size(); i ++){
+          if(!visited[rooms[f][i]]){
+            roomsLeft --;
+            q.push(rooms[f][i]);
+            visited[rooms[f][i]] = true;
+          }
+        }
+      }
+      return roomsLeft == 0;
+    }
+};
+// @lc code=end
+
