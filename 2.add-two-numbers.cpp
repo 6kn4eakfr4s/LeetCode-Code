dfd7fa7108f96d7573c43faaece2aba2baef7b6f
Jesse Chu <jesse.chu@outlook.com>
2026-01-06 06:57:51 +0800

    LeetCode #2

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -16,10 +16,38 @@
  * };
  */
 class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+ public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* ptr_1 = l1;
+    ListNode* ptr_2 = l2;
+    int num_1;
+    int num_2;
+    int sum;
+    int carry = 0;
+    ListNode* dummy = new ListNode();
+    ListNode* result_ptr = dummy;
+    while (ptr_1 != nullptr || ptr_2 != nullptr) {
+      if (ptr_1 == nullptr) {
+        num_1 = 0;
+      } else {
+        num_1 = ptr_1->val;
+        ptr_1 = ptr_1->next;
+      }
+      if (ptr_2 == nullptr) {
+        num_2 = 0;
+      } else {
+        num_2 = ptr_2->val;
+        ptr_2 = ptr_2->next;
+      }
+      sum = (num_1 + num_2 + carry) % 10;
+      carry = (num_1 + num_2 + carry) / 10;
+      result_ptr->next = new ListNode(sum);
+      result_ptr = result_ptr->next;
     }
+    if (carry != 0) {
+      result_ptr->next = new ListNode(carry);
+    }
+    return dummy->next;
+  }
 };
 // @lc code=end
-
